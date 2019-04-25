eb33392fc1257572b6f6f0c79b1b93e95baa122f
Lauren Pardee <lap5151@gmail.com>
2019-04-25 17:17:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,8 +17,8 @@ class App < Sinatra::Base
       "#{params[:phrase]}"
     end
     @number = params[:number].to_i
-    say
-    @number.to_s
+    say*@number
+    #@number.to_s
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
