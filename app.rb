44463dbcc023b944f8e9b76632db6c57cc3ee6d4
Lauren Pardee <lap5151@gmail.com>
2019-04-25 16:46:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    params[:number].times do {params[:phrase]}
+    3.times do {params[:phrase]}
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
